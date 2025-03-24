8ad1d7a995c180919e31676f2a6bfee1cedb34aa
eselemu <emi.f.cass@gmail.com>
2025-03-24 15:52:48 -0600

    +Server concurrency

--- server/server.c
+++ server/server.c
@@ -7,20 +7,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/wait.h>
 
 #define MSG_SIZE 2048
 #define PORT 5000
 
-int sd; // Socket descriptor
+int sd;
 
-// Signal handler for graceful shutdown
 void aborta_handler(int sig) {
     printf("\nServer shutting down...\n");
     close(sd);
     exit(0);
 }
 
-// Function to trim newline characters from a string
 void trim_newline(char *str) {
     int len = strlen(str);
     if (len > 0 && str[len - 1] == '\n') {
@@ -31,34 +30,72 @@ void trim_newline(char *str) {
     }
 }
 
+void handle_client(int client_sd, struct sockaddr_in client_addr) {
+    char msg[MSG_SIZE];
+    
+    printf("Child process %d handling client %s:%d\n", 
+           getpid(), inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    
+    while (1) {
+        int bytes_received = recv(client_sd, msg, sizeof(msg), 0);
+        if (bytes_received <= 0) {
+            if (bytes_received == 0) {
+                printf("Client %s:%d disconnected\n", 
+                       inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            } else {
+                perror("Receive failed");
+            }
+            break;
+        }
+
+        msg[bytes_received] = '\0';
+        trim_newline(msg);
+        printf("Received from client %s:%d: %s\n", 
+               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), msg);
+
+        if (strcmp(msg, "close") == 0) {
+            printf("Client %s:%d requested to close\n", 
+                   inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            break;
+        }
+
+        char response[MSG_SIZE + 10];
+        snprintf(response, sizeof(response), "Message: %s", msg);
+        
+        if (send(client_sd, response, strlen(response), 0) == -1) {
+            perror("Send failed");
+            break;
+        }
+    }
+
+    close(client_sd);
+    printf("Child process %d exiting\n", getpid());
+    exit(0);
+}
+
 int main() {
-    // Set up signal handler for Ctrl+C (SIGINT)
     if (signal(SIGINT, aborta_handler) == SIG_ERR) {
         perror("Could not set signal handler");
         return 1;
     }
 
-    // Create a TCP socket
     if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         return 1;
     }
 
-    // Define server address
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT); // Port number
-    server_addr.sin_addr.s_addr = INADDR_ANY; // Listen on all interfaces
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind the socket to the address
     if (bind(sd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("Bind failed");
         close(sd);
         return 1;
     }
 
-    // Listen for incoming connections
-    if (listen(sd, 5) == -1) { // Backlog of 5
+    if (listen(sd, 5) == -1) {
         perror("Listen failed");
         close(sd);
         return 1;
@@ -66,59 +103,31 @@ int main() {
 
     printf("Server is listening on port %d...\n", PORT);
 
-    // Accept a client connection
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    int client_sd = accept(sd, (struct sockaddr*)&client_addr, &client_len);
-    if (client_sd == -1) {
-        perror("Accept failed");
-        close(sd);
-        return 1;
-    }
-
-    printf("Client connected from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
-    char msg[MSG_SIZE];
     while (1) {
-        // Receive a message from the client
-        int bytes_received = recv(client_sd, msg, sizeof(msg), 0);
-        if (bytes_received == -1) {
-            perror("Receive failed");
-            break;
-        } else if (bytes_received == 0) {
-            printf("Client disconnected\n");
-            break;
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        int client_sd = accept(sd, (struct sockaddr*)&client_addr, &client_len);
+        
+        if (client_sd == -1) {
+            perror("Accept failed");
+            continue;
         }
 
-        msg[bytes_received] = '\0'; // Null-terminate the received message
-        trim_newline(msg); // Trim newline characters
-        printf("Received from client: %s\n", msg);
-
-        // Check if the client sent "close"
-        if (strcmp(msg, "close") == 0) {
-            printf("Client requested to close the connection\n");
-            break;
+        pid_t pid = fork();
+        if (pid == 0) { // Child process
+            close(sd); // Close listening socket in child
+            handle_client(client_sd, client_addr);
+        } else if (pid > 0) { // Parent process
+            close(client_sd); // Close client socket in parent
+            printf("Created child process %d for new connection\n", pid);
+            
+            while (waitpid(-1, NULL, WNOHANG) > 0);
+        } else {
+            perror("Fork failed");
+            close(client_sd);
         }
-
-        // Prepare the response: "Message: " + received message
-        char response[MSG_SIZE + 10]; // Extra space for "Message: "
-        snprintf(response, sizeof(response), "Message: %s", msg);
-
-        // Send the response back to the client
-        if (send(client_sd, response, strlen(response), 0) == -1) {
-            perror("Send failed");
-            break;
-        }
-        printf("Sent to client: %s\n", response);
     }
 
-    // Close the client socket
-    close(client_sd);
-    printf("Client connection closed\n");
-
-    // Close the server socket
     close(sd);
-    printf("Server shutdown complete\n");
-
     return 0;
 }
